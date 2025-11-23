ed712b7b84158dcf08a6923ec0f355f82c9be8bd
reetikarana <reetikarana555@gmail.com>
2025-11-23 10:43:59 +0545

    Update q11.c

--- src/q11.c
+++ src/q11.c
@@ -1,1 +1,6 @@
+float calculateAverage(int arr[], int n) {
+    int sum = 0;
+    for(int i=0;i<n;i++) sum += arr[i];
+    return (float)sum / n;
+}
 // Write a function named calculateAverage that takes an array of integers as input and returns the average of the numbers.
