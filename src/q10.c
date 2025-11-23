06692abcb740e7d02af4537f15892b5b894db0e6
reetikarana <reetikarana555@gmail.com>
2025-11-23 10:38:53 +0545

    Update q10.c

--- src/q10.c
+++ src/q10.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char a[100], b[100];
+    int i=0, j=0;
+
+    gets(a);
+    gets(b);
+
+    while(a[i] != '\0') i++;
+
+    while(b[j] != '\0')
+        a[i++] = b[j++];
+
+    a[i] = '\0';
+
+    puts(a);
+    return 0;
+}
 // Write a C program to concatenate two strings without using the built-in string functions.
