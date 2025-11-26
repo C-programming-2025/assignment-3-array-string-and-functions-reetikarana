8048b61144d5f3f389dccf63f44e297a5185a8c2
Reetika Rana <reetikarana555@gmail.com>
2025-11-26 10:24:08 +0545

    testing

--- src/q1.c
+++ src/q1.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
-
 int main() {
     int n;
-
     printf("Enter the number of elements: ");
     scanf("%d", &n);
-
     int arr[n];
-
-    printf("Enter the elements:\n");
-    for(int i = 0; i < n; i++) {
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
-    int max = arr[0];
-    int min = arr[0];
-
-    for(int i = 1; i < n; i++) {
-        if(arr[i] > max)
-            max = arr[i];
-        if(arr[i] < min)
-            min = arr[i];
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
     }
-
-    printf("Maximum element = %d\n", max);
-    printf("Minimum element = %d\n", min);
-
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
--- src/q2.c
+++ src/q2.c
@@ -1,1 +1,36 @@
-// Implement a C program to reverse the elements of an array.
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    // Input size of array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array
+    for (int i = 0; i < n / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    // Output reversed array
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
